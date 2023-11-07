7491195597cc77e58d0505bf1fa54575e8a2b394
HKLeeeee <dlapdlf1739@gmail.com>
2023-11-07 18:01:08 +0900

    feature: [BE] exception filter 작성
    예외 처리 관련 기능
    - custom exception : httpException 또는 Error를 상속받은 custom exception 클래스를 미리 생성 후 사용
    - error filter : 애플리케이션에서 발생되는 모든 타입의 에러를 필터링하여 json 형태로 응답을 줌. 예상하지 못한 경우의 에러는 모두 500에러로 처리

--- backEnd/api/src/main.ts
+++ backEnd/api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TimeoutInterceptor } from './interceptor/timeout.intercetor';
 import { ConfigService } from '@nestjs/config';
+import { ErrorFilter } from './exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,5 +11,6 @@ async function bootstrap() {
 
   const port = configService.get<number>('PORT');
   await app.listen(port);
+  app.useGlobalFilters(new ErrorFilter());
 }
 bootstrap();
