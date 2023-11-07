5456c2db5cd6f764d59590df7801273e9d264d2f
Ji Seungmin <cswcsm02@gmail.com>
2023-11-07 15:45:19 +0900

    feat: api 서버 시간 측정 인터셉터 기능 구현 - 슬랙에 api 요청 처리 시간 측정 메시지 전송

--- backEnd/api/src/app.module.ts
+++ backEnd/api/src/app.module.ts
@@ -2,11 +2,25 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './logger/logger.middleware';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TimeoutInterceptor } from './interceptor/timeout.intercetor';
+import { SlackModule } from 'nestjs-slack-webhook';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SlackModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'webhook',
+          url: configService.get<string>('WEB_HOOK_URL'),
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TimeoutInterceptor],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
--- backEnd/api/src/main.ts
+++ backEnd/api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TimeoutInterceptor } from './interceptor/timeout.intercetor';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  app.useGlobalInterceptors(new TimeoutInterceptor(configService));
+
   await app.listen(3000);
 }
 bootstrap();
