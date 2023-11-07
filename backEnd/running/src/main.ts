299675990c948920c2f046bfc767767b568a4a89
Ji Seungmin <cswcsm02@gmail.com>
2023-11-07 15:49:29 +0900

    feat: running 서버 환경 세팅 - timeout 인터셉터를 통해 요청 처리 시간 메시지 전송 기능 구현 - logger 미들웨어를 통해 로깅 기능 구현

--- backEnd/running/src/main.ts
+++ backEnd/running/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { TimeoutInterceptor } from './interceptor/timeout.intercetor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  app.useGlobalInterceptors(new TimeoutInterceptor(configService));
   await app.listen(3000);
 }
 bootstrap();
