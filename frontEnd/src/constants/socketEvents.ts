8c44a2f372e2a7c062e21a1e20ff386eee3a090f
d0422 <rlfehd2013@naver.com>
2023-11-09 16:13:43 +0900

    feat: [FE] socket이벤트 설정을 완료한 socket을 반환하는 createSocket함수 구현 createSocket함수 구현 socket 통신에 사용되는 이벤트는 constants를 통해 상수로 관리 socket 통신에 사용되는 url역시 constants를 통해 상수로 관리

---
+++ frontEnd/src/constants/socketEvents.ts
@@ -0,0 +1,12 @@
+export const SOCKET_RECEIVE_EVENT = {
+  ALL_USERS: 'all_users',
+  OFFER: 'getOffer',
+  ANSWER: 'getAnswer',
+  CANDIDATE: 'getCandidate',
+  USER_EXIT: 'user_exit',
+};
+
+export const SOCKET_EMIT_EVENT = {
+  OFFER: 'offer',
+  ANSWER: 'answer',
+};
