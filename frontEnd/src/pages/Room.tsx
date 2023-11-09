4dd3d3baf8dbfbc0eed58cad91d77af8a7916ea2
d0422 <rlfehd2013@naver.com>
2023-11-09 16:16:22 +0900

    feat: [FE] Room 페이지 비디오, 오디오 공유 기능 추가 Room 페이지 컴포넌트 구성 VideoStream을 받아서 보여주는 컴포넌트 구성

--- frontEnd/src/pages/Room.tsx
+++ frontEnd/src/pages/Room.tsx
@@ -1,3 +1,34 @@
+import { RTCConnectionList, createSocket } from '@/services/Socket';
+import { useEffect, useRef } from 'react';
+
+function StreamVideo({ stream }: { stream: MediaStream }) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    (videoRef.current as HTMLVideoElement).srcObject = stream;
+  }, []);
+
+  return <video ref={videoRef}></video>;
+}
+
 export default function Room() {
-  return <div>Room</div>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+      })
+      .then((video) => {
+        (videoRef.current as HTMLVideoElement).srcObject = video;
+        createSocket(video);
+      });
+  }, []);
+
+  return (
+    <div>
+      <video ref={videoRef} muted autoPlay></video>
+      {Object.entries(RTCConnectionList).map(([key, value]) => value.stream && <StreamVideo key={key} stream={value.stream} />)}
+    </div>
+  );
 }
