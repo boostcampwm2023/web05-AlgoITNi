eba8286db6965ef467de4fb87e6a57d44ce30a16
d0422 <rlfehd2013@naver.com>
2023-11-13 22:26:45 +0900

    chore: [FE] tailwind 커스텀 색상 추가 mainColor,backgroundColor,black,white 설정

--- frontEnd/tailwind.config.js
+++ frontEnd/tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      mainColor: '#37485D',
+      backgroundColor: '#F0F2F5',
+      black: '#0C151C',
+      white: '#FFFFFF',
+    },
+    fontFamily: {
+      Pretendard: 'Pretendard-Regular',
+    },
   },
   plugins: [],
 };
