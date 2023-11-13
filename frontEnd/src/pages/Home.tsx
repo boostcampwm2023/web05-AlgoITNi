1cc3a5776cd37767f20ce2717ce567da34489f41
d0422 <rlfehd2013@naver.com>
2023-11-13 22:30:03 +0900

    feat: [FE] Home 화면 UI 구성 Home화면 UI구성 (#32)

--- frontEnd/src/pages/Home.tsx
+++ frontEnd/src/pages/Home.tsx
@@ -1,3 +1,25 @@
+import Button from '@/components/Button';
+
 export default function Home() {
-  return <div>Home</div>;
+  const handleClick = () => {};
+
+  return (
+    <div className="h-[100vh] w-[100vw] bg-backgroundColor flex justify-center items-center py-[10vw] ">
+      <div className="flex flex-col basis-[50%] gap-[130px] justify-center items-center">
+        <div>
+          <div className="text-[8.8vw] font-bold font-Pretendard ">AlgoITNi</div>
+          <div className="text-[1.4vw]  font-Pretendard">AlgoITNi를 통해 동료, 친구와 함께 알고리즘을 학습해봐요!</div>
+        </div>
+        <div className="flex gap-[14px]">
+          <Button onClick={handleClick} fontSize="1.8vw">
+            방생성
+          </Button>
+          <input className="rounded-[15px] font-Pretendard px-[1.6vw] py-[14px] text-[1.8vw]" />
+        </div>
+      </div>
+      <div className="basis-[50%] max-w-[500px] max-h-[500px] ">
+        <img src="/main.png" alt="main" />
+      </div>
+    </div>
+  );
 }
