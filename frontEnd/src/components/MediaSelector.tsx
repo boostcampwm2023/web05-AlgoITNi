ea2295a39874ff9fc90e3b4c233cf26d89a3e90d
d0422 <rlfehd2013@naver.com>
2023-11-14 18:53:19 +0900

    feat: [FE] media목록을 보여주고 media를 결정하는 MediaSelector 구현 media목록을 보여주고 media를 결정하는 MediaSelector 구현 (#45)

---
+++ frontEnd/src/components/MediaSelector.tsx
@@ -0,0 +1,20 @@
+export default function MediaSelector({
+  optionsData,
+  setFunc,
+}: {
+  optionsData: MediaDeviceInfo[];
+  setFunc: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFunc(e.target.value);
+  };
+  return (
+    <select className="w-[33%] font-Pretendard text-xl" onChange={onChange}>
+      {optionsData?.map((device) => (
+        <option key={device.label} value={device.deviceId} className="font-Pretendard">
+          {device.label}
+        </option>
+      ))}
+    </select>
+  );
+}
