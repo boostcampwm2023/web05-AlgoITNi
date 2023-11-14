4c4a00d45c7e0032fa7c4a7f1d54f9076a266117
Ji Seungmin <cswcsm02@gmail.com>
2023-11-14 17:08:47 +0900

    fix: [BE] 부하 테스트를 위해 인터셉터 코드 제거

--- backEnd/running/src/main.ts
+++ backEnd/running/src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { TimeoutInterceptor } from './common/interceptor/timeout.intercetor';
 import { ErrorFilter } from './common/exception/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  app.useGlobalInterceptors(new TimeoutInterceptor(configService));
   app.useGlobalFilters(new ErrorFilter());
 
   const origin = configService.get<string>('ALLOWED_ORIGIN');
