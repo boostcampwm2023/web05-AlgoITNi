3f96b1532cc1f53364b355b4a21223652301fd82
d0422 <rlfehd2013@naver.com>
2023-11-15 17:21:52 +0900

    feat: [FE] 변경된 speaker를 적용시키는 부분 구현 변경된 speaker를 video에 설정하는 부분 구현 (#45)

--- frontEnd/src/components/Video.tsx
+++ frontEnd/src/components/Video.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useRef } from 'react';
+import useSpeaker from '@/stores/useSpeaker';
 
 export default function Video({ stream, muted = false }: { stream: MediaStream; muted?: boolean }) {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const speaker = useSpeaker((state) => state.speaker);
 
   useEffect(() => {
     if (!videoRef.current) return;
 
     videoRef.current.srcObject = stream;
+    // setSinkId가 experimental method라서 typescript에 type이 없어서 이렇게 사용하였음
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (videoRef.current as any).setSinkId(speaker);
   }, [stream]);
 
   return (
