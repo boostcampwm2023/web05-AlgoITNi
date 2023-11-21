40860140611b87a9c6d552fdc41c32bedc557c86
d0422 <rlfehd2013@naver.com>
2023-11-22 01:39:11 +0900

    feat: [FE] 공통 모달 애니메이션 적용 공통 모달 애니메이션 적용

---
+++ frontEnd/src/components/common/Modal.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
+import cancelImg from '@/assets/cancel.svg';
+
+const ANIMATION_RENDER = 'relative p-4 bg-white rounded-2xl animate-render';
+const ANIMATION_REMOVE = 'relative p-4 bg-white rounded-2xl animate-remove';
+
+export default function Modal({
+  Component,
+  modalProps,
+  cancel,
+}: {
+  Component: React.ComponentType<any>;
+  modalProps?: Record<string, unknown>;
+  cancel: () => void;
+}) {
+  const [className, setClassName] = useState(ANIMATION_RENDER);
+  const handleCancel = () => {
+    setClassName(ANIMATION_REMOVE);
+  };
+  const handleAnimationEnd = () => {
+    if (className === ANIMATION_REMOVE) cancel();
+  };
+
+  return (
+    <div
+      className="z-20 absolute top-0 left-0 flex items-center justify-center w-screen h-screen bg-[rgba(0,0,0,0.5)]"
+      onClick={handleCancel}
+    >
+      <div className={className} onClick={(e) => e.stopPropagation()} onAnimationEnd={handleAnimationEnd}>
+        <button className="absolute w-4 h-4 top-4 right-4" onClick={handleCancel} type="button">
+          <img src={cancelImg} alt="cancel" />
+        </button>
+        <div className="py-8">
+          <Component {...{ ...modalProps, hide: handleCancel }} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontEnd/tailwind.config.js
+++ frontEnd/tailwind.config.js
@@ -3,6 +3,20 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      keyframes: {
+        render: {
+          '0%': { transform: 'scale(0)' },
+          '100%': { transform: 'scale(1)' },
+        },
+        remove: {
+          '0%': { transform: 'scale(1)' },
+          '100%': { transform: 'scale(0)' },
+        },
+      },
+      animation: {
+        render: 'render 0.3s',
+        remove: 'remove 0.3s',
+      },
       colors: {
         mainColor: '#37485D',
         backgroundColor: '#F0F2F5',
