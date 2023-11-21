a5df418e7a25dd86b0544a1ec8a80dd29848dfa0
d0422 <rlfehd2013@naver.com>
2023-11-22 00:34:14 +0900

    chore: [FE] 입장 전 설정시 Video 크기를 60vh로 변경하여 비율 개선 입장 전 설정시 Video 크기를 60vh로 변경하여 비율 개선

--- frontEnd/src/components/setting/SettingVideo.tsx
+++ frontEnd/src/components/setting/SettingVideo.tsx
@@ -17,7 +17,7 @@ export default function SettingVideo({ mediaObject }: { mediaObject: MediaObject
   return (
     stream && (
       <div className="flex flex-col gap-[20px] w-full h-full">
-        <div className="relative w-full h-[550px]">
+        <div className="relative w-full h-[60vh]">
           <Video stream={stream} muted />
           <div className="absolute flex items-center justify-center w-full gap-[1vw] bottom-[10px]">
             <MediaControlButton
