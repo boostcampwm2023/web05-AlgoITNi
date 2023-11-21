ceca3c5928e51ad9898deb18d4f1a407a7168ac8
d0422 <rlfehd2013@naver.com>
2023-11-22 00:31:13 +0900

    feat: [FE] 비디오 세팅할 수 있는 기능 추가 캠을 고르고 적용 클릭시에 Room에 적용시키는 기능 추가 (#85)

--- frontEnd/src/components/room/modal/SettingModal.tsx
+++ frontEnd/src/components/room/modal/SettingModal.tsx
@@ -5,23 +5,31 @@ import { SettingType } from '@/types/settingModal';
 import SideBar from './setting/SideBar';
 import AudioSetting from './setting/AudioSetting';
 import Button from '@/components/common/Button';
+import VideoSetting from './setting/VideoSetting';
 
 export default function SettingModal({ mediaObject, hide }: { mediaObject: MediaObject; hide: () => void }) {
   const [settingType, setSettingType] = useState<SettingType>('audio');
   const {
     mic: { setMic },
+    camera: { setCamera },
   } = mediaObject;
   const setSpeaker = useSpeaker((state) => state.setSpeaker);
-  const { stream, mic, speaker } = useMedia();
+  // settings 모달만의 mediaStream을 새롭게 생성
+  const { stream, mic, speaker, camera } = useMedia();
+  const cameraProps = { list: camera.list, setFunc: camera.setCamera };
   const micProps = { list: mic.list, setFunc: mic.setMic };
   const speakerProps = { list: speaker.list, setFunc: setSpeaker };
+
+  // settings 모달의 mediaStream의 정보를 토대로 room Stream의 정보를 갱신
   const applyStreamToRealStream = () => {
     if (!stream) return;
-    const [micTrack] = stream.getAudioTracks();
-
-    setMic(micTrack.getSettings().deviceId as string);
+    const [settingMicTrack] = stream.getAudioTracks();
+    const [settingVideoTrack] = stream.getVideoTracks();
+    setMic(settingMicTrack.getSettings().deviceId as string);
+    setCamera(settingVideoTrack.getSettings().deviceId as string);
     hide();
   };
+
   return (
     <div className="flex flex-col items-center justiy-center">
       <div className="flex w-[50vw] h-[60vh] font-Pretendard">
@@ -30,7 +38,7 @@ export default function SettingModal({ mediaObject, hide }: { mediaObject: Media
           {settingType === 'audio' ? (
             <AudioSetting stream={stream as MediaStream} mic={micProps} speaker={speakerProps} />
           ) : (
-            <div>video</div>
+            <VideoSetting stream={stream as MediaStream} camera={cameraProps} />
           )}
         </div>
       </div>
