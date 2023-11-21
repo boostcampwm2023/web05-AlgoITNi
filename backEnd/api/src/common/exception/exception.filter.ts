ca360f7eb117257b4f1b0f0e503c1e06d6dca486
HKLeeeee <dlapdlf1739@gmail.com>
2023-11-21 11:25:06 +0900

    refactor: [BE] exception filter 분리 exception 종류별로 각각 필터를 다르게 분리하였음

--- backEnd/api/src/common/exception/exception.filter.ts
+++ backEnd/api/src/common/exception/exception.filter.ts
@@ -4,29 +4,63 @@ import {
   ArgumentsHost,
   HttpException,
   Logger,
+  HttpStatus,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { JsonWebTokenError } from 'jsonwebtoken';
 
-@Catch(Error)
-export class ErrorFilter implements ExceptionFilter {
-  private readonly logger = new Logger('Error Filter');
-  catch(exception: Error, host: ArgumentsHost) {
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger('HttpException Filter');
+  catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const status =
-      exception instanceof HttpException ? exception.getStatus() : 500;
-    const message =
-      exception instanceof HttpException
-        ? exception.message
-        : 'Internal Serval Error';
+    const status = exception.getStatus();
     this.logger.error(
       `path : ${request.url} \n${exception.message}`,
       exception,
     );
     response.status(status).json({
       statusCode: status,
-      message,
+      message: exception.message,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
+
+@Catch(JsonWebTokenError)
+export class AuthErrorFilter implements ExceptionFilter {
+  private readonly logger = new Logger('AuthError Filter');
+  catch(exception: JsonWebTokenError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+    this.logger.error(
+      `path : ${request.url} \n${exception.message}`,
+      exception,
+    );
+    response.status(HttpStatus.UNAUTHORIZED).json({
+      statusCode: HttpStatus.UNAUTHORIZED,
+      message: exception.message,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
+
+@Catch()
+export class ErrorFilter implements ExceptionFilter {
+  private readonly logger = new Logger('Error Filter');
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = HttpStatus.INTERNAL_SERVER_ERROR;
+    const message = 'Internal server error';
+    this.logger.error(`path : ${request.url} \n${message}`, exception);
+    response.status(status).json({
+      statusCode: status,
+      message: message,
       timestamp: new Date().toISOString(),
     });
   }
--- backEnd/api/src/main.ts
+++ backEnd/api/src/main.ts
@@ -2,20 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TimeoutInterceptor } from './common/interceptor/timeout.intercetor';
 import { ConfigService } from '@nestjs/config';
-import { ErrorFilter } from './common/exception/exception.filter';
-import { ValidationPipe } from '@nestjs/common';
+import {
+  HttpExceptionFilter,
+  AuthErrorFilter,
+  ErrorFilter,
+} from './common/exception/exception.filter';
 import { WinstonLogger } from './common/logger/winstonLogger.service';
-
+import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.useGlobalInterceptors(new TimeoutInterceptor(configService));
-  app.useGlobalFilters(new ErrorFilter());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
+
+  app.useGlobalFilters(
+    new ErrorFilter(), // last
+    new HttpExceptionFilter(),
+    new AuthErrorFilter(), // first
   );
+
   // const origin = configService.get<string>('ALLOWED_ORIGIN');
   app.useLogger(app.get(WinstonLogger));
 
@@ -23,6 +27,8 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
+
+  app.use(cookieParser());
 
   const port = configService.get<number>('PORT');
   await app.listen(port);
