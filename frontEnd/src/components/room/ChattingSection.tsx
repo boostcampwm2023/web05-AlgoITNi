39a9a408f72f20d70c10cf3a56f5ae1b36931f72
HBSPS <wiyeong79@naver.com>
2023-11-22 16:38:09 +0900

    feat: [FE] 채팅 말풍선 구성 채팅 말풍선 및 전체 메시지 state 구성 (#89)

--- frontEnd/src/components/room/ChattingSection.tsx
+++ frontEnd/src/components/room/ChattingSection.tsx
@@ -2,16 +2,23 @@ import { useEffect, useState } from 'react';
 import { Socket, io } from 'socket.io-client/debug';
 import { VITE_CHAT_URL } from '@/constants/env';
 
+interface SocketMessage {
+  message: string;
+}
+
 let socket: Socket;
 
 export default function ChattingSection({ roomId }: { roomId: string }) {
   const [message, setMessage] = useState('');
+  const [allMessages, setAllMessage] = useState<SocketMessage[]>([]);
 
   useEffect(() => {
     socket = io(VITE_CHAT_URL, {
       transports: ['websocket'],
     });
-    socket.on('new_message', (recievedMessage) => console.log(recievedMessage));
+    socket.on('new_message', (recievedMessage) => {
+      setAllMessage((prev) => [...prev, JSON.parse(recievedMessage)]);
+    });
     socket.connect();
 
     socket.emit('join_room', { room: roomId });
@@ -27,8 +34,18 @@ export default function ChattingSection({ roomId }: { roomId: string }) {
   };
 
   return (
-    <div className="relative flex items-center justify-center w-full h-full rounded-lg bg-primary min-w-[150px]">
-      <form onSubmit={handleMessageSend} className="absolute bottom-0 w-full p-2 rounded-b-lg">
+    <div className="relative p-2 flex items-center justify-center w-full h-full rounded-lg bg-primary min-w-[150px]">
+      <div className="flex flex-col w-full h-full gap-2 px-2">
+        {allMessages.map((messageData, index) => (
+          <div key={index} className="flex flex-col gap-0.5">
+            <span className="text-xs font-light text-white">보낸사람</span>
+            <div className="px-4 py-2 bg-white rounded-lg w-fit">
+              <span>{messageData.message}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleMessageSend} className="absolute bottom-0 w-full p-2 rounded-b-lg bg-primary">
         <div className="flex items-center h-10 border rounded-lg border-base">
           <input
             type="text"
