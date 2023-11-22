ebcbad247d2048df452dcd3a9c6e4af6938f653a
d0422 <rlfehd2013@naver.com>
2023-11-22 14:54:57 +0900

    feat: [FE] QuizViewSection 클릭 아이콘 추가 + 멘트 변경 QuizViewSection 클릭 아이콘 추가 + 멘트 변경 (#91)

--- frontEnd/src/components/room/QuizViewSection.tsx
+++ frontEnd/src/components/room/QuizViewSection.tsx
@@ -1,5 +1,6 @@
 import useModal from '@/hooks/useModal';
 import LinkInputModal from './modal/LinkInputModal';
+import clickSrc from '@/assets/click.svg';
 
 export default function QuizViewSection() {
   const { show, hide } = useModal(LinkInputModal);
@@ -9,7 +10,10 @@ export default function QuizViewSection() {
       className="flex items-center justify-center w-full h-full text-white rounded-lg bg-mainColor font-Pretendard"
       onClick={() => show({ hide })}
     >
-      <div>링크를 입력해주세요</div>
+      <div className="flex gap-2">
+        <img src={clickSrc} width="20px" alt="clickIcon" />
+        <div>클릭해서 링크 입력하기</div>
+      </div>
     </button>
   );
 }
