c628861f680fe4702a4af62fd9a0f5eba54c8c7a
Ji Seungmin <cswcsm02@gmail.com>
2023-11-22 12:04:33 +0900

    fix: [BE] 채팅 서버 websockerAdapter 적용

--- backEnd/chat/src/main.ts
+++ backEnd/chat/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useWebSocketAdapter(new IoAdapter(app));
 
   app.enableCors({
     origin: true,
