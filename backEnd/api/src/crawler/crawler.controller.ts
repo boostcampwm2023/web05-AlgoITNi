f083912f9aed1fa7fed3c3b8d1ae102364f9762f
Ji Seungmin <cswcsm02@gmail.com>
2023-11-23 18:12:32 +0900

    feat: [BE] 크롤링 배포 API 기능 구현

--- backEnd/api/src/crawler/crawler.controller.ts
+++ backEnd/api/src/crawler/crawler.controller.ts
@@ -5,6 +5,12 @@ import { CrawlerService } from './crawler.service';
 export class CrawlerController {
   constructor(private readonly crawlerService: CrawlerService) {}
 
+  @Get()
+  async crawling(@Query('url') url: string) {
+    const content = await this.crawlerService.findOne(url);
+    return content;
+  }
+
   @Get('v1')
   async crawlingV1(@Query('url') url: string) {
     const content = await this.crawlerService.findOne(url);
@@ -13,6 +19,7 @@ export class CrawlerController {
 
   @Get('v2')
   async crawlingV2(@Query('url') url: string) {
+    console.log('요청이 들어왔습니다.');
     const content = await this.crawlerService.findOneUsingCache(url);
     return content;
   }
