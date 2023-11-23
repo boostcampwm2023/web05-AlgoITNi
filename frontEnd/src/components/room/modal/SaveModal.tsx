c788643a50d27938f889285b1878aed33173428f
d0422 <rlfehd2013@naver.com>
2023-11-23 15:18:07 +0900

    feat: [FE] 값이 없으면 파일이 저장되지 않는 기능 추가, enter를 입력해도 제출하는 기능추가 값이 없으면 파일이 저장되지 않는 기능 추가 enter를 입력해도 제출하는 기능추가 (#103)

--- frontEnd/src/components/room/modal/SaveModal.tsx
+++ frontEnd/src/components/room/modal/SaveModal.tsx
@@ -14,6 +14,10 @@ export default function SaveModal({ hide, code }: { hide: () => void; code: stri
   const { show: showLoginModal, hide: hideLoginModal } = useModal(LoginModal);
 
   const handleClick = async () => {
+    if (!inputValue) {
+      if (ref.current) ref.current.focus();
+      return;
+    }
     hide();
     try {
       await postUserCode(inputValue, code);
@@ -24,14 +28,26 @@ export default function SaveModal({ hide, code }: { hide: () => void; code: stri
       }
     }
   };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => e.key === 'Enter' && handleClick();
+
   return (
     <div className="flex flex-col gap-4">
       <div className="text-xl">저장될 코드의 파일 이름을 입력해주세요</div>
       <div className="flex items-center justify-center gap-4">
-        <input ref={ref} value={inputValue} onChange={onChange} className="px-4 py-2 text-xl" placeholder="solution.py" />
-        <Button.Default onClick={handleClick} fontSize="1vw">
-          저장하기
-        </Button.Default>
+        <input
+          ref={ref}
+          value={inputValue}
+          onChange={onChange}
+          onKeyDown={handleKeyDown}
+          className="px-4 py-2 text-xl"
+          placeholder="solution.py"
+        />
+        <div className={inputValue ? '' : 'opacity-50'}>
+          <Button.Default onClick={handleClick} fontSize="1vw">
+            저장하기
+          </Button.Default>
+        </div>
       </div>
     </div>
   );
