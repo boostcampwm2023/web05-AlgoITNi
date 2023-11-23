83cb9de32d0f12e1aeb4dbf672ee009172517a81
d0422 <rlfehd2013@naver.com>
2023-11-23 18:03:34 +0900

    chore: 리뷰 반영 변경된 스타일 룰 적용

--- frontEnd/src/components/room/editor/LoadButton.tsx
+++ frontEnd/src/components/room/editor/LoadButton.tsx
@@ -36,10 +36,10 @@ export default function LoadButton({ setCode }: { setCode: (value: React.SetStat
   };
   return (
     <div className="relative h-full">
-      <div className="peer flex items-center min-w-[8vh] justify-center px-[max(2vh,25px)] h-full text-[max(1.2vh,10px)] bg-[#132A37] font-thin text-white rounded whitespace-nowrap">
+      <div className="peer flex items-center min-w-[8vh] justify-center px-[max(2vh,25px)] h-full text-[max(1.2vh,10px)] bg-secondary font-thin text-white rounded whitespace-nowrap">
         불러오기
       </div>
-      <div className="absolute z-10 items-center justify-between hidden gap-2 p-2 -translate-x-1/2 rounded-lg bg-[#132A37] left-1/2 -top-12 peer-hover:flex hover:flex">
+      <div className="absolute z-10 items-center justify-between hidden gap-2 p-2 -translate-x-1/2 rounded-lg bg-secondary left-1/2 -top-12 peer-hover:flex hover:flex">
         <LoadButtonElement onClick={handleLoadLocalCodeFile}>로컬에서 불러오기</LoadButtonElement>
         <LoadButtonElement onClick={handleLoadCloudCodeFile}>클라우드에서 불러오기</LoadButtonElement>
       </div>
