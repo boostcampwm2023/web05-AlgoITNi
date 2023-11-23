ffe3a1940bd93ea807036adef99131bca0372343
HBSPS <wiyeong79@naver.com>
2023-11-23 13:29:19 +0900

    refactor: [FE] 채팅 컴포넌트 분리 채팅 컴포넌트 분리 및 커스텀 훅 분리 (#89)

--- frontEnd/src/components/room/ChattingSection.tsx
+++ frontEnd/src/components/room/ChattingSection.tsx
@@ -1,20 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
 import { Socket, io } from 'socket.io-client/debug';
 import { VITE_CHAT_URL } from '@/constants/env';
-
-interface SocketMessage {
-  message: string;
-}
+import { MessageData } from '@/types/chatting';
+import ChattingMessage from './chatting/ChattingMessage';
+import useScroll from '@/hooks/useScroll';
+import ChattingInput from './chatting/ChattingInput';
+import ScrollDownButton from './chatting/ScrollDownButton';
 
 let socket: Socket;
 
 export default function ChattingSection({ roomId }: { roomId: string }) {
   const [message, setMessage] = useState('');
-  const [allMessages, setAllMessage] = useState<SocketMessage[]>([]);
-
-  const [scrollRatio, setScrollRatio] = useState(100);
-  const [isLastMessageView, setIsLastMessageView] = useState(true);
-  const [isRecievedMessage, setIsRecievedMessage] = useState(false);
+  const [allMessages, setAllMessage] = useState<MessageData[]>([]);
+  const { setIsRecievedMessage, setScrollRatio, isRecievedMessage, isLastMessageView } = useScroll();
 
   const messageAreaRef = useRef<HTMLDivElement | null>(null);
 
@@ -35,13 +33,6 @@ export default function ChattingSection({ roomId }: { roomId: string }) {
     else setIsRecievedMessage(true);
   }, [allMessages]);
 
-  useEffect(() => {
-    if (scrollRatio >= 95) {
-      setIsLastMessageView(true);
-      setIsRecievedMessage(false);
-    } else setIsLastMessageView(false);
-  }, [scrollRatio]);
-
   const handleScroll = () => {
     // TODO: 쓰로틀링 걸기
     if (!messageAreaRef.current) return;
@@ -55,6 +46,10 @@ export default function ChattingSection({ roomId }: { roomId: string }) {
     setScrollRatio(100);
   };
 
+  const handleInputMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  };
+
   const handleMessageSend = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -65,39 +60,14 @@ export default function ChattingSection({ roomId }: { roomId: string }) {
   };
 
   return (
-    <div className="flex flex-col p-2 items-center justify-center w-full h-full rounded-lg bg-primary min-w-[150px]">
-      <div ref={messageAreaRef} className="flex flex-col w-full h-full gap-2 px-2 overflow-auto grow" onScroll={handleScroll}>
+    <div className="flex relative flex-col items-center justify-center w-full h-full rounded-lg bg-primary min-w-[150px]">
+      <div ref={messageAreaRef} className="flex flex-col w-full h-full gap-2 px-2 pt-2 overflow-auto grow" onScroll={handleScroll}>
         {allMessages.map((messageData, index) => (
-          <div key={index} className="flex flex-col gap-0.5">
-            <span className="text-xs font-light text-white">보낸사람</span>
-            <div className="px-4 py-2 bg-white rounded-lg w-fit">
-              <span>{messageData.message}</span>
-            </div>
-          </div>
+          <ChattingMessage messageData={messageData} key={index} />
         ))}
       </div>
-      <form onSubmit={handleMessageSend} className="relative w-full p-2 rounded-b-lg bg-primary">
-        {isRecievedMessage && (
-          <button
-            type="button"
-            onClick={handleMoveToBottom}
-            className="absolute z-10 w-8 h-8 text-xs text-white rounded-full bg-secondary right-8 -top-10"
-          >
-            ↓
-          </button>
-        )}
-        <div className="flex items-center h-10 border rounded-lg border-base">
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="w-full h-10 p-2 rounded-s-lg focus:outline-none"
-          />
-          <button type="submit" className="h-full px-4 py-1 font-light text-white rounded-e-lg whitespace-nowrap bg-secondary">
-            전송
-          </button>
-        </div>
-      </form>
+      {isRecievedMessage && <ScrollDownButton handleMoveToBottom={handleMoveToBottom} />}
+      <ChattingInput handleMessageSend={handleMessageSend} message={message} handleInputMessage={handleInputMessage} />
     </div>
   );
 }
