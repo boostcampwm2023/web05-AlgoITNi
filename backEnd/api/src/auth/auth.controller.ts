e4bd19454ac4132884f84d1734bf031315654cc3
HKLeeeee <dlapdlf1739@gmail.com>
2023-11-26 23:43:30 +0900

    feature: [BE] google OAuth 유저 정보 가져오기

--- backEnd/api/src/auth/auth.controller.ts
+++ backEnd/api/src/auth/auth.controller.ts
@@ -15,17 +15,72 @@ import { UsersService } from '../users/users.service';
 import { UserInfoDto } from '../users/dto/userInfo.dto';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './auth.guard';
-import * as path from 'path';
-
+import { google } from 'googleapis';
+import { ConfigService } from '@nestjs/config';
 @Controller('auth')
 export class AuthController {
   private logger = new Logger(AuthController.name);
+  private oauth2Client;
   constructor(
     private githubService: GithubService,
     private googleService: GoogleService,
     private authService: AuthService,
     private userService: UsersService,
-  ) {}
+    private configService: ConfigService,
+  ) {
+    this.oauth2Client = new google.auth.OAuth2(
+      this.configService.get<string>('CLIENT_ID_GOOGLE'),
+      this.configService.get<string>('CLIENT_SECRET_GOOGLE'),
+      'http://localhost:4000/auth/google-callback',
+    );
+  }
+
+  @Get('google')
+  async googleProxy(@Res() res: Response, @Req() req: Request) {
+    const scopes = [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
+    ];
+    const authorizationUrl = this.oauth2Client.generateAuthUrl({
+      // 'online' (default) or 'offline' (gets refresh_token)
+      access_type: 'offline',
+      /** Pass in the scopes array defined above.
+       * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
+      scope: scopes,
+      // Enable incremental authorization. Recommended as a best practice.
+      include_granted_scopes: true,
+    });
+
+    return res.redirect(authorizationUrl);
+  }
+
+  @Get('google-callback')
+  async googleCallback(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Query('code') code: string,
+  ) {
+    const { tokens } = await this.oauth2Client.getToken(code);
+
+    const { id_token } = tokens;
+    const segments = id_token.split('.');
+    // All segment should be base64
+    const headerSeg = segments[0];
+    const payloadSeg = segments[1];
+    function base64urlUnescape(str) {
+      str += Array(5 - (str.length % 4)).join('=');
+      return str.replace(/\-/g, '+').replace(/_/g, '/');
+    }
+    function base64urlDecode(str) {
+      return new Buffer(base64urlUnescape(str), 'base64').toString();
+    }
+
+    // base64 decode and parse JSON
+    const header = JSON.parse(base64urlDecode(headerSeg));
+    const payload = JSON.parse(base64urlDecode(payloadSeg));
+    console.log(header, payload);
+    return res.redirect('http://localhost:3000');
+  }
 
   @Get('github')
   async githubProxy(
