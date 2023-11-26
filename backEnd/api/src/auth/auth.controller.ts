ddc33c268409075c700beeb29c2fc1b07582738b
HKLeeeee <dlapdlf1739@gmail.com>
2023-11-27 00:04:50 +0900

    feature: [BE] google 로그인 - 회원가입

--- backEnd/api/src/auth/auth.controller.ts
+++ backEnd/api/src/auth/auth.controller.ts
@@ -15,43 +15,23 @@ import { UsersService } from '../users/users.service';
 import { UserInfoDto } from '../users/dto/userInfo.dto';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './auth.guard';
-import { google } from 'googleapis';
-import { ConfigService } from '@nestjs/config';
+import { UserDto } from '../users/dto/user.dto';
+
 @Controller('auth')
 export class AuthController {
   private logger = new Logger(AuthController.name);
-  private oauth2Client;
   constructor(
     private githubService: GithubService,
     private googleService: GoogleService,
     private authService: AuthService,
     private userService: UsersService,
-    private configService: ConfigService,
-  ) {
-    this.oauth2Client = new google.auth.OAuth2(
-      this.configService.get<string>('CLIENT_ID_GOOGLE'),
-      this.configService.get<string>('CLIENT_SECRET_GOOGLE'),
-      'http://localhost:4000/auth/google-callback',
-    );
-  }
+  ) {}
 
   @Get('google')
   async googleProxy(@Res() res: Response, @Req() req: Request) {
-    const scopes = [
-      'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.googleapis.com/auth/userinfo.profile',
-    ];
-    const authorizationUrl = this.oauth2Client.generateAuthUrl({
-      // 'online' (default) or 'offline' (gets refresh_token)
-      access_type: 'offline',
-      /** Pass in the scopes array defined above.
-       * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
-      scope: scopes,
-      // Enable incremental authorization. Recommended as a best practice.
-      include_granted_scopes: true,
-    });
-
-    return res.redirect(authorizationUrl);
+    req.session['returnTo'] = req.headers.referer || '/';
+    const redirectUrl = this.googleService.getAuthUrl();
+    return res.redirect(redirectUrl);
   }
 
   @Get('google-callback')
@@ -60,26 +40,26 @@ export class AuthController {
     @Res() res: Response,
     @Query('code') code: string,
   ) {
-    const { tokens } = await this.oauth2Client.getToken(code);
-
-    const { id_token } = tokens;
-    const segments = id_token.split('.');
-    // All segment should be base64
-    const headerSeg = segments[0];
-    const payloadSeg = segments[1];
-    function base64urlUnescape(str) {
-      str += Array(5 - (str.length % 4)).join('=');
-      return str.replace(/\-/g, '+').replace(/_/g, '/');
+    const user: UserDto = await this.googleService.getIDToken(code);
+    let findUser = await this.userService.findUser(user);
+    if (findUser === null) {
+      await this.userService.addUser(user, 'google');
+      findUser = await this.userService.findUser(user);
     }
-    function base64urlDecode(str) {
-      return new Buffer(base64urlUnescape(str), 'base64').toString();
+    this.logger.debug(findUser);
+    if (findUser.oauth !== 'google') {
+      return { message: '다른 서비스로 가입한 내역이 있습니다.' }; // TODO: set StatusCode
     }
 
-    // base64 decode and parse JSON
-    const header = JSON.parse(base64urlDecode(headerSeg));
-    const payload = JSON.parse(base64urlDecode(payloadSeg));
-    console.log(header, payload);
-    return res.redirect('http://localhost:3000');
+    const loginUser = new UserInfoDto();
+    loginUser.id = findUser.id;
+    loginUser.name = findUser.name;
+
+    await this.authService.login(loginUser, res);
+
+    const returnTo = req.session['returnTo'];
+    delete req.session['returnTo'];
+    return res.redirect(returnTo);
   }
 
   @Get('github')
@@ -106,7 +86,7 @@ export class AuthController {
     @Query('code') code: string,
   ) {
     const accessToken = await this.githubService.getGithubAccessToken(code);
-    const user = await this.githubService.getUserInfo(accessToken);
+    const user: UserDto = await this.githubService.getUserInfo(accessToken);
     let findUser = await this.userService.findUser(user);
     if (findUser === null) {
       await this.userService.addUser(user, 'github');
