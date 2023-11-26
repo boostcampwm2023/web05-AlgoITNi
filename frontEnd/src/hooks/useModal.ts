af413cac38944f17f9331fcf989364e901389267
d0422 <rlfehd2013@naver.com>
2023-11-26 23:27:32 +0900

    refactor: [FE] useModal을 오버로드하여 모달 내외부에서 적절한 행동을 하도록 변경 모달 내부에서 인자없이 useModal을 사용할 경우 hide만 반환 모달 외부에서 인자로 모달요소를 넣어서 사용하는경우 show와 hide를 모두 반환하도록 처리 (#118)

--- frontEnd/src/hooks/useModal.ts
+++ frontEnd/src/hooks/useModal.ts
@@ -2,25 +2,36 @@ import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import useModalState, { MODAL_COMPONENTS } from '@/stores/useModalState';
 
-export default function useModal<P extends Record<string, unknown>>(Component: React.ComponentType<P>) {
-  const { addModal, showModal, hideModal } = useModalState((state) => state);
+type CalledByModalInner = { hide: () => void };
+type CalledByModalOuter<P> = { show: (props?: P) => void; hide: () => void };
+
+function useModal(): CalledByModalInner;
+function useModal<P extends Record<string, unknown>>(Component?: React.ComponentType<P>): CalledByModalOuter<P>;
+
+function useModal<P extends Record<string, unknown>>(Component?: React.ComponentType<P>): CalledByModalInner | CalledByModalOuter<P> {
+  const { showModal, hideModal, modals } = useModalState((state) => state);
   const [modalId] = useState(uuidv4());
 
   useEffect(() => {
+    if (!Component) return;
     MODAL_COMPONENTS[modalId] = {
       Component,
     };
-    addModal(modalId);
   }, [modalId]);
 
   const show = (props?: P) => {
+    if (!Component) return;
     MODAL_COMPONENTS[modalId].props = props;
     showModal(modalId);
   };
 
   const hide = () => {
-    hideModal(modalId);
+    if (Component) hideModal(modalId);
+    else hideModal(modals[modals.length - 1]);
   };
 
-  return { show, hide };
+  if (Component) return { show, hide };
+  return { hide };
 }
+
+export default useModal;
