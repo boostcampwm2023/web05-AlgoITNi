623d79cc920a80d10fa06a1b1149d8fc5f1720a3
d0422 <rlfehd2013@naver.com>
2023-11-27 17:16:34 +0900

    feat: [FE] 코드 목록에서 삭제하는 기능 추가 코드 목록에서 삭제하는 기능 추가 (#133)

--- frontEnd/src/components/room/modal/codeList/CodeFileButton.tsx
+++ frontEnd/src/components/room/modal/codeList/CodeFileButton.tsx
@@ -1,4 +1,11 @@
+import { useMutation } from '@tanstack/react-query';
 import { LoadCodeData } from '@/types/loadCodeData';
+import cancelIcon from '@/assets/cancelIcon.svg';
+import useModal from '@/hooks/useModal';
+import WarningModal from '../WarningModal';
+import deleteUserCode from '@/apis/deleteUserCodes';
+import reactQueryClient from '@/configs/reactQueryClient';
+import QUERY_KEYS from '@/constants/queryKeys';
 
 export default function CodeFileButton({
   code,
@@ -11,8 +18,23 @@ export default function CodeFileButton({
   selectOne: string;
   setSelectOne: React.Dispatch<React.SetStateAction<string>>;
 }) {
+  const { show } = useModal(WarningModal);
+  const { mutate } = useMutation({
+    mutationFn: (id: string) => deleteUserCode(id),
+    onSuccess: () => reactQueryClient.invalidateQueries({ queryKey: [QUERY_KEYS.LOAD_CODES] }),
+  });
+
   const handleClick = () => {
     if (code.id !== selectOne) setSelectOne(code.id);
+  };
+
+  const handleDeleteButton = () => {
+    show({
+      warningString: '선택한 파일이 삭제됩니다.',
+      callback: () => {
+        mutate(code.id);
+      },
+    });
   };
 
   return (
@@ -22,7 +44,14 @@ export default function CodeFileButton({
       onDoubleClick={handleDoubleClick}
       className="flex flex-col items-center justify-center col-span-1"
     >
-      <img src="/fileIcon.png" alt="fileIcon" className="w-1/3" />
+      <div className="relative w-1/3 group">
+        <img src="/fileIcon.png" alt="fileIcon" />
+
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+        <div className="absolute -top-3 -right-2 w-[50px] group-hover:block hidden" onClick={handleDeleteButton}>
+          <img src={cancelIcon} alt="cancel" />
+        </div>
+      </div>
       <div>{code.title}</div>
     </button>
   );
