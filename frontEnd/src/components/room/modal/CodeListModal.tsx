e7293a6a6bb2413dbb9b3782c9e8fa5097297be4
d0422 <rlfehd2013@naver.com>
2023-11-27 22:23:21 +0900

    fix: [FE] 파일 불러오기 모달이 안뜨는 이슈 해결시도 파일 불러오기 모달이 안뜨는 이슈 해결시도 useQuery를 modal 내부에서 실행하는 형태로 이슈 해결시도

--- frontEnd/src/components/room/modal/CodeListModal.tsx
+++ frontEnd/src/components/room/modal/CodeListModal.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { LoadCodeData } from '@/types/loadCodeData';
 import CodeFileButton from './codeList/CodeFileButton';
 import Button from '@/components/common/Button';
 import useModal from '@/hooks/useModal';
 import WarningModal from './WarningModal';
+import getUserCodes from '@/apis/getUserCodes';
+import QUERY_KEYS from '@/constants/queryKeys';
 
 export default function CodeListModal({
   codeData,
@@ -12,12 +15,17 @@ export default function CodeListModal({
   codeData: LoadCodeData[];
   setPlainCode: (value: React.SetStateAction<string>) => void;
 }) {
+  const { data } = useQuery({
+    queryKey: [QUERY_KEYS.LOAD_CODES],
+    queryFn: getUserCodes,
+  });
+
   const [selectOne, setSelectOne] = useState<string>('');
   const { show } = useModal(WarningModal);
   const { hide } = useModal();
 
   const findById = (id: string) => {
-    const result = codeData.find((data) => data.id === id);
+    const result = codeData.find((code) => code.id === id);
     return result;
   };
 
@@ -28,13 +36,13 @@ export default function CodeListModal({
       show({ warningString: '작업중이던 내용이 모두 지워집니다.', callback: () => setPlainCode(result.content) });
     }
   };
-
+  const renderData = data || codeData;
   return (
     <div className="flex flex-col items-center justify-center w-full gap-4">
       <div className="w-full text-xl font-bold ">파일 불러오기</div>
       <div className="w-[70vw] h-[50vh] max-h-[50vh] overflow-y-auto border-y p-4">
         <div className="grid grid-cols-5 gap-4">
-          {codeData.map((code) => (
+          {renderData.map((code) => (
             <div key={code.id} className={code.id === selectOne ? 'bg-blue-100  rounded-xl py-4' : 'py-4'}>
               <CodeFileButton code={code} handleDoubleClick={handleClick} selectOne={selectOne} setSelectOne={setSelectOne} />
             </div>
