cf1d28e14f1f2c1b81e7024c227b90b561bd021e
d0422 <rlfehd2013@naver.com>
2023-11-28 20:48:44 +0900

    chore: [FE] 로딩 컴포넌트 개선 로딩 컴포넌트 개선 (#151)

--- frontEnd/src/components/room/quizView/Loading.tsx
+++ frontEnd/src/components/room/quizView/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full p-4 mx-auto border rounded-md shadow bg-primary">
+    <div className="flex flex-col items-center justify-center w-full h-full px-12 border rounded-md shadow mx4-auto bg-primary">
       <div className="flex flex-col justify-center w-full">
         <div className="flex flex-col justify-center gap-8 animate-pulse">
           <div className="h-3 rounded bg-slate-200" />
@@ -13,6 +13,11 @@ export default function Loading() {
               </div>
               <div className="h-3 rounded bg-slate-200" />
               <div className="h-3 rounded bg-slate-200" />
+            </div>
+            <div className="h-3 rounded bg-slate-200" />
+            <div className="grid grid-cols-3 gap-4">
+              <div className="h-3 col-span-1 rounded bg-slate-200" />
+              <div className="h-3 col-span-2 rounded bg-slate-200" />
             </div>
           </div>
         </div>
