0422daef1b079dbf88247a9d5d087376f53c7129
d0422 <rlfehd2013@naver.com>
2023-11-29 16:34:41 +0900

    feat: [FE] 코드 저장 api에 언어를 입력받도록 변경 코드 저장 api에 언어를 입력받도록 변경 (#146)

--- frontEnd/src/apis/postUserCode.ts
+++ frontEnd/src/apis/postUserCode.ts
@@ -1,8 +1,10 @@
+import { Language } from '@/types/editor';
 import AuthRequest from './AuthRequest';
 
-export default function postUserCode(title: string, content: string) {
+export default function postUserCode(title: string, content: string, language: Language) {
   return AuthRequest.post('/codes', {
     title,
     content,
+    language,
   });
 }
