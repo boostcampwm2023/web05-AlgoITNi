0818813488f721d297dc0285064ba5bf59ec93a9
d0422 <rlfehd2013@naver.com>
2023-11-30 13:59:53 +0900

    feat: Modal 렌더링 최적화 Zustand의 전역상태가 변화하면 actions함수보 바뀌어서 전부 렌더링이 다시되던 문제 해결 (#173)

--- frontEnd/src/hooks/useModal.ts
+++ frontEnd/src/hooks/useModal.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useCallback } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import useModalState, { MODAL_COMPONENTS } from '@/stores/useModalState';
 import { ModalHideContext } from '@/components/modal/Modal';
@@ -10,7 +10,7 @@ function useModal(): CalledByModalInner;
 function useModal<P extends Record<string, unknown>>(Component?: React.ComponentType<P>): CalledByModalOuter<P>;
 
 function useModal<P extends Record<string, unknown>>(Component?: React.ComponentType<P>): CalledByModalInner | CalledByModalOuter<P> {
-  const { showModal, hideModal } = useModalState((state) => state);
+  const { showModal, hideModal } = useModalState((state) => state.actions);
   const [modalId] = useState(uuidv4());
   // Modals에서 주입시킨 Provider로부터 Modal에 애니메이션을 적용시키는 함수 Context를 가져온다.
   const hideThisModal = useContext(ModalHideContext);
@@ -22,16 +22,16 @@ function useModal<P extends Record<string, unknown>>(Component?: React.Component
     };
   }, [modalId]);
 
-  const show = (props?: P) => {
+  const show = useCallback((props?: P) => {
     if (!Component) return;
     MODAL_COMPONENTS[modalId].props = props;
     showModal(modalId);
-  };
+  }, []);
 
-  const hide = () => {
+  const hide = useCallback(() => {
     if (Component) hideModal(modalId);
     else hideThisModal();
-  };
+  }, []);
 
   if (Component) return { show, hide };
   return { hide };
