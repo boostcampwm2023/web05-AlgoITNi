fe6949545968ae72d155d709b2260592a12eb655
d0422 <rlfehd2013@naver.com>
2023-12-04 17:07:03 +0900

    fix: [FE] 선택한 장비가 정확하게 보이지 않는 이슈 해결 선택한 장비가 정확하게 보이지 않는 이슈 해결 (#111)

--- frontEnd/src/components/setting/MediaSelector.tsx
+++ frontEnd/src/components/setting/MediaSelector.tsx
@@ -1,8 +1,10 @@
 export default function MediaSelector({
+  stream,
   optionsData,
   setFunc,
   className,
 }: {
+  stream: MediaStream;
   optionsData: MediaDeviceInfo[];
   setFunc: React.Dispatch<React.SetStateAction<string>>;
   className?: string;
@@ -10,8 +12,15 @@ export default function MediaSelector({
   const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setFunc(e.target.value);
   };
+
+  const [videoTrack] = stream.getVideoTracks();
+  const [audioTrack] = stream.getAudioTracks();
+  const video = optionsData.find((option) => option.label === videoTrack.label);
+  const audioInput = optionsData.find((option) => option.label === audioTrack.label);
+  const value = video || audioInput;
+
   return (
-    <select className={className} onChange={onChange}>
+    <select className={className} onChange={onChange} value={value?.deviceId}>
       {optionsData?.map((device) => {
         return (
           <option key={device.label} value={device.deviceId} className="font-Pretendard">
