dc6e06d919e1e790f505dd149464e6dbf6b0262c
d0422 <rlfehd2013@naver.com>
2023-12-04 17:30:55 +0900

    refactor: Modal Input Form으로 변경 Modal Input Form으로 변경 (#111)

--- frontEnd/src/components/room/modal/LinkInputModal.tsx
+++ frontEnd/src/components/room/modal/LinkInputModal.tsx
@@ -7,29 +7,34 @@ export default function LinkInputModal({ setURL }: { setURL: React.Dispatch<Reac
   const { inputValue, onChange } = useInput('');
   const { hide } = useModal();
 
-  const handleOK = () => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!inputValue && inputRef.current) return inputRef.current.focus();
+
     setURL(inputValue);
-    hide();
+    return hide();
   };
 
   return (
     <div className="flex flex-col justify-between h-full pb-4 font-Pretendard ">
-      <input
-        ref={inputRef}
-        value={inputValue}
-        onChange={onChange}
-        type="text"
-        placeholder="URL을 입력해주세요"
-        className="px-4 py-2 mx-2 mb-2 text-xl"
-      />
-      <div className="absolute bottom-0 left-0 flex w-full border justify-evenly rounded-b-2xl">
-        <button type="button" className="w-full py-2 text-xl border-r-2" onClick={hide}>
-          취소
-        </button>
-        <button type="button" className="w-full py-2 text-xl font-bold text-mainColor" onClick={handleOK}>
-          확인
-        </button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          ref={inputRef}
+          value={inputValue}
+          onChange={onChange}
+          type="text"
+          placeholder="URL을 입력해주세요"
+          className="px-4 py-2 mx-2 mb-2 text-xl"
+        />
+        <div className="absolute bottom-0 left-0 flex w-full border justify-evenly rounded-b-2xl">
+          <button type="button" className="w-full py-2 text-xl border-r-2" onClick={hide}>
+            취소
+          </button>
+          <button type="submit" className="w-full py-2 text-xl font-bold text-mainColor">
+            확인
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
