e3030bbbf8102f4e03afa076cdd582fbc9227d82
d0422 <rlfehd2013@naver.com>
2023-12-07 13:29:15 +0900

    refactor: [FE] useRoomConfigData에 connection 관련 상태, 오류 상태 추가 useRoomConfigData에 connection 관련 상태, 오류 상태 추가 (#211)

--- frontEnd/src/stores/useRoomConfigData.ts
+++ frontEnd/src/stores/useRoomConfigData.ts
@@ -1,12 +1,19 @@
 import { create } from 'zustand';
 
 interface RoomConfigData {
+  isConnectionDone: boolean;
   isSettingDone: boolean;
+  isConnectionError: boolean;
+  isSignalingError: boolean;
   nickname: string;
   actions: {
+    finishConnection: () => void;
+    stopConnection: () => void;
     settingOn: () => void;
     settingOff: () => void;
     setNickname: (value: string) => void;
+    throwSignalError: () => void;
+    throwConnectionError: () => void;
   };
 }
 
@@ -14,12 +21,19 @@ const defaultCode = localStorage.getItem('code');
 const defaultNickName = localStorage.getItem('nickName');
 
 const useRoomConfigData = create<RoomConfigData>((set) => ({
+  isConnectionDone: false,
   isSettingDone: (!!defaultCode || defaultCode === '') && !!defaultNickName,
+  isSignalingError: false,
+  isConnectionError: false,
   nickname: defaultNickName || '',
   actions: {
+    finishConnection: () => set((state) => ({ ...state, isConnectionDone: true })),
+    stopConnection: () => set((state) => ({ ...state, isConnectionDone: false })),
     settingOn: () => set((state) => ({ ...state, isSettingDone: true })),
     settingOff: () => set((state) => ({ ...state, isSettingDone: false })),
     setNickname: (value) => set((state) => ({ ...state, nickname: value })),
+    throwSignalError: () => set((state) => ({ ...state, isSignalingError: true })),
+    throwConnectionError: () => set((state) => ({ ...state, isConnectionError: true })),
   },
 }));
 
