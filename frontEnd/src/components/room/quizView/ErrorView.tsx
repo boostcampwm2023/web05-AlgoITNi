2542afc5e53a731978d32c845ca62b3d98cb6235
d0422 <rlfehd2013@naver.com>
2023-12-08 21:57:53 +0900

    chore: [FE] 불필요한 console.log삭제 불필요한 console.log삭제

--- frontEnd/src/components/room/quizView/ErrorView.tsx
+++ frontEnd/src/components/room/quizView/ErrorView.tsx
@@ -1,7 +1,6 @@
 import { isAxiosError } from 'axios';
 
 export default function ErrorView({ error }: { error: Error }) {
-  console.log(error);
   if (isAxiosError(error) && error.response && error.response.status === 500) {
     return <div className="font-bold text-point-red animate-[vibration_.5s_linear]">오류가 발생했습니다!</div>;
   }
