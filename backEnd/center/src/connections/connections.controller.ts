303806805023f56e814ba7ab50003050a4f513f7
Ji Seungmin <cswcsm02@gmail.com>
2023-12-12 19:33:53 +0900

    feat: [BE] 채팅 컨트롤러 구현

--- backEnd/center/src/connections/connections.controller.ts
+++ backEnd/center/src/connections/connections.controller.ts
@@ -2,10 +2,14 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { JoinRoomDto } from './dto/join-room.dto';
 import { EventsService } from 'src/events/events.service';
 import { ResponseDto } from 'src/common/dto/common-response.dto';
+import { EventsChatService } from 'src/events/events-chat.service';
 
 @Controller('connections')
 export class ConnectionsController {
-  constructor(private readonly eventService: EventsService) {}
+  constructor(
+    private readonly eventService: EventsService,
+    private readonly eventChatService: EventsChatService,
+  ) {}
 
   @Post('join/signalling')
   findSignalingServer(@Body() data: JoinRoomDto): ResponseDto {
@@ -21,13 +25,13 @@ export class ConnectionsController {
 
   @Post('join/chatting')
   findChattingServer(@Body() data: JoinRoomDto): ResponseDto {
-    const response: ResponseDto = this.eventService.findServer(data);
+    const response: ResponseDto = this.eventChatService.findServer(data);
     return response;
   }
 
   @Post('leave/chatting')
   leaveChatting(@Body() data: JoinRoomDto): ResponseDto {
-    const response: ResponseDto = this.eventService.leaveRoom(data);
+    const response: ResponseDto = this.eventChatService.leaveRoom(data);
     return response;
   }
 }
